08dd4739f908cfbfbe59966f443802c24d35f51c
SriLalitha-007 <kotilalitha7@gmail.com>
2023-09-22 14:07:57 +0530

    Initial setup

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+export default function LoginPage(){
+    return (
+       <div>
+        <form> 
+            <input type="email" placeholder='your@email.com'/>
+            <input type="Password" placeholder='password'/>
+            <button> Login </button>
+        </form>
+       </div>
+    );
+}
